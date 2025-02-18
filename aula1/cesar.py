20ba48a90fb0d274ec9059c203e74f180abf257e
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-02-18 13:44:43 +0000

    'Cesar's Cypher' Exercise

--- aula1/cesar.py
+++ aula1/cesar.py
@@ -1,15 +1,39 @@
 import sys
 
+def preproc(str):
+ l = []
+ for c in str:
+    if c.isalpha():
+        l.append(c.upper())
+ return "".join(l) 
+
+
 
 def encrypt(secret_key, message):
-    l = []
+    msg = ""
+    start = ord('A')
+    key = ord(secret_key) - start  # Convert key character to ASCII
+    #print("Secret_key ord: " + str(key))
+    
     for c in message:
-        if c.isalpha():
-            l.append(c.upper())
-    "".join(l)
+        dif = (start + (ord(c) - start + key) %26)
+        msg += chr(dif)
+
+    return msg  # Return the encrypted message
+        
     
 def decipher(secret_key, message):
-    print(secret_key, message)
+    msg = ""
+    start = ord('A')
+    key = ord(secret_key) - start  # Convert key character to ASCII
+    #print("Secret_key ord: " + str(key))
+    
+    for c in message:
+        dif = dif = start + (ord(c) - start - key) % 26
+        msg += chr(dif)
+        #print(msg)
+
+    return msg  # Return the encrypted message
 
 
 
@@ -17,13 +41,17 @@ def main(argc, argv):
     if(argc < 4):
         print("USAGE: python3 cesar.py <operation> <secret_key> <message>")
     elif(argc == 4):
+        
         operation = argv[1].lower()
         secret_key = argv[2]
-        message = argv[3]
+        message = preproc(argv[3])
+        
         if operation == "enc":
-            encrypt(secret_key, message)
+            msg = encrypt(secret_key, message)
+            print(msg)
         elif operation == "dec":
-            decipher(secret_key, message)
+            msg1 = decipher(secret_key, message)
+            print(msg1)
         
     
 
